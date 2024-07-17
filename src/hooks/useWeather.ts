224e454d51a51bd43580795c9b90f1b11e5e1fb3
juanlunaa <jp3165239@gmail.com>
2024-07-17 17:53:46 -0500

    Add custom hook useWeather to use zustand store in App

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+import { useWeatherStore } from "../store/weather"
+
+export const useWeather = () => {
+  const city = useWeatherStore((state) => state.city)
+  const country = useWeatherStore((state) => state.country)
+  const apiResponse = useWeatherStore((state) => state.apiResponse)
+  const initialCity = useWeatherStore((state) => state.initialCity)
+  const setCity = useWeatherStore((state) => state.setCity)
+  const error = useWeatherStore((state) => state.error)
+
+  useEffect(() => {
+    initialCity()
+  }, [])
+
+  return { city, country, data: apiResponse, setCity, error }
+}
