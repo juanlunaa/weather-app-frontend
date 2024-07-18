5e2252638e77ead60daf0d66edf24f8778f7ecb9
juanlunaa <jp3165239@gmail.com>
2024-07-18 15:55:43 -0500

    Code formatting by prettier

--- src/services/cityUser.ts
+++ src/services/cityUser.ts
@@ -1,18 +1,18 @@
-import { IPInfoAPIResponse } from "../types"
+import { IPInfoAPIResponse } from "../types";
 
 export const fecthCityUser = async () => {
-  try {
-    const url = `https://ipinfo.io?token=${import.meta.env.VITE_IP_INFO_API_KEY}`
-    const res = await fetch(url)
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`)
-    }
+	try {
+		const url = `https://ipinfo.io?token=${import.meta.env.VITE_IP_INFO_API_KEY}`;
+		const res = await fetch(url);
+		if (!res.ok) {
+			throw new Error(`HTTP error! Status: ${res.status}`);
+		}
 
-    const json = await res.json() as IPInfoAPIResponse
+		const json = (await res.json()) as IPInfoAPIResponse;
 
-    return json
-  } catch (error) {
-    console.log("Error ", error)
-    return null
-  }
-}
+		return json;
+	} catch (error) {
+		console.log("Error ", error);
+		return null;
+	}
+};
--- src/store/weather.ts
+++ src/store/weather.ts
@@ -1,29 +1,31 @@
-import { create } from "zustand"
-import { WeatherResponse } from "../types"
-import { fecthCityUser } from "../services/cityUser"
+import { create } from "zustand";
+import { WeatherResponse } from "../types";
+import { fecthCityUser } from "../services/cityUser";
 import mockData from "../mocks/data.json";
 
 interface State {
-  city: string
-  country: string
-  apiResponse: WeatherResponse
-  initialCity: () => void
-  setCity: (city: string) => void
-  error: string
+	city: string;
+	country: string;
+	apiResponse: WeatherResponse;
+	initialCity: () => void;
+	setCity: (city: string) => void;
+	error: string;
 }
 
 export const useWeatherStore = create<State>()((set) => {
-  return{
-    city: "",
-    country: "",
-    apiResponse: mockData,
-    initialCity: async () => {
-      const json = await fecthCityUser()
-      set({ city: json.city, country: json.country })
-    },
-    setCity: (city) => {
-      set({ city })
-    },
-    error: ""
-  }
-})
+	return {
+		city: "",
+		country: "",
+		apiResponse: mockData,
+		initialCity: async () => {
+			const json = await fecthCityUser();
+			if (json != null) {
+				set({ city: json.city, country: json.country });
+			}
+		},
+		setCity: (city) => {
+			set({ city });
+		},
+		error: "",
+	};
+});
