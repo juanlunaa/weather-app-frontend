a953d4fd7ea7b84d742dca412dab81c5254441cd
juanlunaa <jp3165239@gmail.com>
2024-07-18 15:54:14 -0500

    Add error handling

--- src/services/cityUser.ts
+++ src/services/cityUser.ts
@@ -1,9 +1,18 @@
 import { IPInfoAPIResponse } from "../types"
 
 export const fecthCityUser = async () => {
-  const url = `https://ipinfo.io?token=${import.meta.env.VITE_IP_INFO_API_KEY}`
-  const res = await fetch(url)
-  const json = await res.json() as IPInfoAPIResponse
+  try {
+    const url = `https://ipinfo.io?token=${import.meta.env.VITE_IP_INFO_API_KEY}`
+    const res = await fetch(url)
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`)
+    }
 
-  return json
+    const json = await res.json() as IPInfoAPIResponse
+
+    return json
+  } catch (error) {
+    console.log("Error ", error)
+    return null
+  }
 }
