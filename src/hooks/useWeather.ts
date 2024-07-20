aeac6915c88b25d7a2fc24463d613d28934217ad
juanlunaa <jp3165239@gmail.com>
2024-07-19 19:45:28 -0500

    Refactor custom hook consumer of the store

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -2,16 +2,14 @@ import { useEffect } from "react";
 import { useWeatherStore } from "../store/weather";
 
 export const useWeather = () => {
-	const city = useWeatherStore((state) => state.city);
-	const country = useWeatherStore((state) => state.country);
 	const apiResponse = useWeatherStore((state) => state.apiResponse);
-	const initialCity = useWeatherStore((state) => state.initialCity);
-	const setCity = useWeatherStore((state) => state.setCity);
+	const initialWeather = useWeatherStore((state) => state.initialWeather);
+	const loading = useWeatherStore((state) => state.loading);
 	const error = useWeatherStore((state) => state.error);
 
 	useEffect(() => {
-		initialCity();
+		initialWeather();
 	}, []);
 
-	return { city, country, data: apiResponse, setCity, error };
+	return { data: apiResponse, loading, error };
 };
