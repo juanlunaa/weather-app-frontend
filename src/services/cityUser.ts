489d451235416175809019d98feaf4b57ed4ae60
juanlunaa <jp3165239@gmail.com>
2024-07-19 19:47:21 -0500

    Change url fetch to backend endpoint

--- src/services/cityUser.ts
+++ src/services/cityUser.ts
@@ -1,18 +1,13 @@
-import { IPInfoAPIResponse } from "../types";
+import { WeatherResponse } from "../types";
 
-export const fecthCityUser = async () => {
-	try {
-		const url = `https://ipinfo.io?token=${import.meta.env.VITE_IP_INFO_API_KEY}`;
-		const res = await fetch(url);
-		if (!res.ok) {
-			throw new Error(`HTTP error! Status: ${res.status}`);
-		}
-
-		const json = (await res.json()) as IPInfoAPIResponse;
-
-		return json;
-	} catch (error) {
-		console.log("Error ", error);
-		return null;
+export const fecthInitialWeather = async () => {
+	const url = "http://127.0.0.1:4000/weather/local";
+	const res = await fetch(url);
+	if (!res.ok) {
+		throw new Error(`HTTP error! Status: ${res.status}`);
 	}
+
+	const json = (await res.json()) as WeatherResponse;
+
+	return json;
 };
